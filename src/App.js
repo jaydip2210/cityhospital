191fd67f77de0854de083f08f014b6007fa4b2ff
jaydip2210 <rathodjaydip86738@gmail.com>
2023-09-19 18:28:09 +0530

    19_9_23_cityhospital

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './container/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import Department from './container/Department/Department';
+import Docters from './container/Docters/Docters';
+import About from './container/About/About';
+import Contact from './container/Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/department' element={<Department />}/>
+        <Route path='/docters' element={<Docters />}/>
+        <Route path='/About' element={<About />}/>
+        <Route path='/Contact' element={<Contact />}/>
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
